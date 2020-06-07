64ac924f426b84d0ad0c4e93a504ed8e6df124f2
hanna.serozhko <serozhkohanna@gmail.com>
2020-06-07 03:18:04 +0300

    CTA section, footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './Footer.scss';
 
+import InstagramIcon from '../../assets/icons/instagram.svg';
+import DribbbleIcon from '../../assets/icons/dribbble.svg';
+import GitHubIcon from '../../assets/icons/github.svg';
+
 const Footer = () => {
   return <footer>
-	footer
+	<ul className='social-block'>
+	  <li className='social-block-item'>
+		<a href="https://www.instagram.com/annat_s/?hl=en">
+		  <img src={InstagramIcon} alt="inst-icon"/>
+		</a>
+	  </li>
+	  <li className='social-block-item'>
+		<a href="https://dribbble.com/annat_s">
+		  <img src={DribbbleIcon} alt="dribble-icon"/>
+		</a>
+	  </li>
+	  <li className='social-block-item'>
+		<a href="https://github.com/serozhkohanna">
+		  <img src={GitHubIcon} alt="github-icon"/>
+		</a>
+	  </li>
+	</ul>
   </footer>
 }
 
