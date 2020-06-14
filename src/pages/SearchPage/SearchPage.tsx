bd271beef738dd5a5e63d7b2efdce840c0c5e453
hanna.serozhko <serozhkohanna@gmail.com>
2020-06-15 02:26:14 +0300

    Added animated inputs.

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import HeroAdvanced from "../../components/HeroAdvanced/HeroAdvanced";
 
 const SearchPage = () => {
   return <main id='search-page'>
-	search page
+	<div className="container">
+	  <HeroAdvanced/>
+	</div>
   </main>
 }
 
