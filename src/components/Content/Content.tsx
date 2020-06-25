a01f2133ddf7acfff7d418fd8475316d3169b0f4
hanna.serozhko <serozhkohanna@gmail.com>
2020-06-26 02:34:34 +0300

    Added list params, display book data.

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -4,21 +4,35 @@ import { connect } from 'react-redux';
 import { ApiResponse } from "../../constants/interfaces";
 
 import SortParams from "../SortParams/SortParams";
+import FilterParams from "../FilterParams/FilterParams";
+import BookItemList from "../BookItemList/BookItemList";
 
 const Content = ({booksList}) => {
   const bookList = booksList.apiResponse;
+  console.log(bookList, 'bookList');
 
-  return <section className='content'>
-	<div className="content-header">
-	  <div className="books-count">
-		<p><span>{bookList.totalItems}</span> items</p>
+  if (booksList.apiResponse.totalItems > 0) {
+	return <section className='content'>
+	  <div className="content-header">
+		<div className="books-count">
+		  <p><span>{bookList.totalItems}</span> items</p>
+		</div>
+		<SortParams/>
 	  </div>
-	  <SortParams/>
-	</div>
-	<div className="content-body">
-	  body
-	</div>
-  </section>
+	  <div className="content-body">
+		<FilterParams/>
+		<div className="books-list">
+		  {booksList.apiResponse.items?.map((item, i) => {
+			return (
+			  <BookItemList key={i} bookItem={item}/>
+			)
+		  })}
+		</div>
+	  </div>
+	</section>
+  } else if (booksList.apiResponse.totalItems === 0) {
+	return <p>no results in this request</p>
+  } else return null;
 }
 
 const mapStateToProps = ({apiResponse}: ApiResponse) => {
